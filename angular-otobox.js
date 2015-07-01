e12ae758f585e60664c6df76b9eaad52fd70cdcd
alihaghighatkhah <alihaghighatkhah@yahoo.com>
2015-07-01 16:09:08 +0430

    removed explicit variable s

--- angular-otobox.js
+++ angular-otobox.js
@@ -1,11 +1,4 @@
 var angularOtobox = angular.module('angular-otobox', []);
-var s = new Array();
-
-angularOtobox.run(['$rootScope', function ($rootScope) {
-  $rootScope.$on("$routeChangeStart", function () {
-    s = new Array();
-  });
-}]);
 
 
 /**
@@ -15,36 +8,42 @@ angularOtobox.run(['$rootScope', function ($rootScope) {
  **/
 angularOtobox.directive("otobox", ['$timeout', '$compile', function ($timeout, $compile) {
   return {
+    scope: {
+      otobox: "="
+    },
     require: "?otoboxConfig",
     link: function ($scope, $element, $attrs, otoboxConfigCtrl) {
       // wating untile template generated
       $timeout(function () {
-        // initiating otobox for the current element
-        s[$attrs.otobox] = new otobox($element[0]);
-
         // adding activator
-        var config = new Object();
-        if ($scope.config instanceof Array) {
+        var config = (otoboxConfigCtrl) ? otoboxConfigCtrl.getConfig() : {};
+        config.key = (config.key) ? config.key : 'otobox';
+        // initiating otobox for the current element
+        $scope.otobox[config.key] = new otobox($element[0]);
+
+        if (config.activators instanceof Array) {
           // Adding multiple activators for a single emenet is only possible using otoboxConfig directive
-          for (var j = 0; j < $scope.config.length; j++) {
-            s[$attrs.otobox].addActivator($scope.config[j].activator);
-            s[$attrs.otobox].setOption('useText', $scope.config.setText);
-          };
+          for (var j = 0; j < config.activators.length; j++) {
+            $scope.otobox[config.key].addActivator(config.activators[j]);
+            $scope.otobox[config.key].setOption('useText', config.activators[j].setText);
+          }
         } else {
-          config = {
-              name: $attrs.otobox,
-              key: $attrs.otoboxKey,
-              customChoice: $attrs.otoboxCustomChoice || false,
-              includeKey: $attrs.otoboxIncludeKey || true,
-              allowedChars: new RegExp($attrs.otoboxAllowedChars),
-              source: $attrs.otoboxSource,
-              displayKey: $attrs.otoboxDisplayKey,
-              valueKey: $attrs.otoboxValueKey,
-              setText: $attrs.setText || true
+
+          config.activators = {
+            name: $attrs.otobox,
+            key: $attrs.otoboxKey,
+            customChoice: $attrs.otoboxCustomChoice || false,
+            includeKey: $attrs.otoboxIncludeKey || true,
+            allowedChars: new RegExp($attrs.otoboxAllowedChars),
+            source: $attrs.otoboxSource,
+            displayKey: $attrs.otoboxDisplayKey,
+            valueKey: $attrs.otoboxValueKey,
+            setText: $attrs.setText || true
           };//config
-          s[$attrs.otobox].setOption('useText', config.setText).addActivator(config);
+
+          $scope.otobox[config.key].setOption('useText', config.activators.setText).addActivator(config.activators);
         }
-        s[$attrs.otobox].bind('update', function () {
+        $scope.otobox[config.key].bind('update', function () {
           $element.triggerHandler('input');
         });
       });
